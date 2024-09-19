036d67d6c60550b8af3cb7b78f8b72ee4ac45499
aaronparker <aaronparker@users.noreply.github.com>
2024-09-19 01:59:51 +0000

    Validate and compress icons: 53

---
+++ enricomi-publish-action-venv/Lib/site-packages/junitparser/__init__.py
@@ -0,0 +1,17 @@
+from .junitparser import (
+    JUnitXmlError,
+    Attr,
+    Element,
+    JUnitXml,
+    TestSuite,
+    Property,
+    Skipped,
+    Failure,
+    Error,
+    TestCase,
+    Properties,
+    IntAttr,
+    FloatAttr,
+)
+
+version = "3.1.2"
